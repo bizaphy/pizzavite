09983f2ab3b513a2562d8258e2d5cfe2520a8f78
Bizaphy <110700808+bizaphy@users.noreply.github.com>
2024-09-03 02:31:06 -0400

    se arreglan bugs leves en 404

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -6,7 +6,14 @@ const NotFound = () => {
           <p className="error404-title">Error 404</p>
           <p className="error404-msj">
             No encontrabamos la pagina que buscabas 😢
+            <Link
+                to="/"
+                className="text-white ms-3 text-decoration-none"
+              >
+                Regresar a Home
+              </Link>
           </p>
+          
         </div>
       </div>
     </>
